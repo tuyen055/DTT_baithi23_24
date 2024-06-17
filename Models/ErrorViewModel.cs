626c87fcd5e8c8be71f8a4cc8e060ff600e7b8d2
tuyen055 <142509972+tuyen055@users.noreply.github.com>
2024-06-17 11:14:50 +0700

    adb

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DoanThiTuyen_0039.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
